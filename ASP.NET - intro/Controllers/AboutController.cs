90a94f4b89b6357db5d98edfa747c3570aa29e2f
RamilA123 <ramilna@code.edu.az>
2023-04-30 09:01:46 +0400

    add folders

---
+++ ASP.NET - intro/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿namespace ASP.NET___intro
+{
+    public class AboutController
+    {
+        public string Index(int id)
+        {
+            return "This a home page";
+        }
+
+        public string Test()
+        {
+            return "This is a test action";
+        }
+    }
+}
---
+++ ASP.NET - intro/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET___intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
